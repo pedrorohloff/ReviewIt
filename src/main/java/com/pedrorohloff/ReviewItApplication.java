661f83f62134730e44625aa0885fb8437b034959
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2026-09-24 15:06:23 -0300

    Initial Commit

---
+++ src/main/java/com/pedrorohloff/ReviewItApplication.java
@@ -0,0 +1,13 @@
+package com.pedrorohloff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrorohloff/ReviewItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrorohloff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
